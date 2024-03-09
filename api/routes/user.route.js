130545dfa7d8b3bc9dab824b9bfea1cdcf330b9a
sebastiang2004 <gabriel.gurzun@student.usv.ro>
2024-03-09 12:17:18 +0200

    Added routes for getUser, updateUser, deleteUser, and assignRole functions in user.route.js

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,6 +3,9 @@ import {
   test,
   updateUser,
   deleteUser,
+  getUser,  
+  updateUser,
+  assignRole,
 } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
@@ -11,5 +14,8 @@ const router = express.Router();
 router.get('/', test);
 router.post('/update/:id', verifyToken, updateUser);
 router.delete('/delete/:id', verifyToken, deleteUser);
+router.get('/:id', getUser);
+router.put('/:id', updateUser);
+router.post('/:id/roles', assignRole);
 
 export default router;
