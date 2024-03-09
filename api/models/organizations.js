c9826a665db0200a2d5d29010963f0faf6e2b053
sebastiang2004 <gabriel.gurzun@student.usv.ro>
2024-03-09 12:13:35 +0200

    Created Organization model with name, address, employees, and identifier fields in organizations.js

--- api/models/organizations.js
+++ api/models/organizations.js
@@ -1,25 +1,25 @@
 import mongoose from 'mongoose';
+import { v4 as uuidv4 } from 'uuid';
 
-const organizationSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    address: {
-      type: String,
-      required: true,
-    },
-    employees: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Employee',
-      },
-    ],
+const organizationSchema = new mongoose.Schema({
+  name: 
+  { type: String, 
+    required: true, 
+    unique: true },
+  address: 
+  { type: String, 
+    required: true 
   },
-  { timestamps: true }
-);
+  employees: 
+  [{
+     type: mongoose.Schema.Types.ObjectId, 
+     ref: 'Employee'
+     }],
+  identifier:
+   {
+     type: String, default: uuidv4 
+    },
+}, { timestamps: true });
 
 const Organization = mongoose.model('Organization', organizationSchema);
 
