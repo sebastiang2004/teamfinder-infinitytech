a84751d7ad248de124537accf6e9a9b16345afe4
sebastiang2004 <gabriel.gurzun@student.usv.ro>
2024-03-09 12:16:33 +0200

    Added routes for getting employee by ID, updating skills, viewing and updating projects in employee.js

--- api/routes/employee.js
+++ api/routes/employee.js
@@ -2,6 +2,7 @@
 
 import express  from "express";
 import{getEmployeeById, updateSkills, viewProjects, updateProjects} from "../controllers/employee.js";
+import { signupEmployee } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
@@ -16,5 +17,8 @@ router.get("/", viewProjects);
 
 // Update employee's projects
 router.put("/", updateProjects);
+
+// Sign Up Employee ref
+router.get('/signup/:orgIdentifier', signupEmployee)
 
 export default router;
